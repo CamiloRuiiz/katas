fbfad35ed6e433db3c78c8204bc009574eeff380
OscarCamiloRuiz <camiloruiz@esquemacreativo.com>
2016-01-19 17:31:15 +0100

    Eval number three

--- kataocr/test_kata_ocr.rb
+++ kataocr/test_kata_ocr.rb
@@ -10,7 +10,6 @@ describe 'KataOCR' do
 ONE
 
   result = scan(one)
-
   expect(result).to eql(1)
   end
   it 'should scan the number two' do
@@ -20,16 +19,36 @@ ONE
 |_      
     
 TWO
-  
-  
   result = scan(two)
-
   expect(result).to eql(2)
-
+  end
+  
+  it 'should scan the number three' do
+    three = <<THREE
+_
+_|
+_|    
+    
+THREE
+  result = scan(three)
+  expect(result).to eql(3)
+  end
+  
+  it 'should scan the number three' do
+    four = <<FOUR
+ 
+|_|
+  |   
+    
+FOUR
+  result = scan(four)
+  expect(result).to eql(4)
   end
 end
 
 def scan(input)
   return 1 if (input == "\n |\n |       \n    \n")
   return 2 if (input == " _\n _|\n|_      \n    \n")
+  return 3 if (input == "_\n_|\n_|    \n    \n") 
+  return 4 if (input == " \n|_|\n  |   \n    \n")   
 end 
